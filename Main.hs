d66846204de56035607b5ac3017d4eeddf622c3a
Manuel Barros Reyes <manuca@gmail.com>
2019-09-05 17:07:52 -0300

    Parse read file

--- Main.hs
+++ Main.hs
@@ -6,18 +6,16 @@ import           Data.Aeson
 import qualified Data.ByteString.Lazy as B
 import           GHC.Generics
 
-fileContents :: B.ByteString
-fileContents =
-  "{\"name\": \"Juan\"}"
-
-data Person = Person { name :: String } deriving Generic
+data Person = Person { first_name :: String } deriving Generic
 
 instance FromJSON Person
 
 main :: IO ()
 main = do
-  case decode fileContents of
+  jsonData <- B.readFile "sample.json"
+
+  case decode jsonData  of
     Nothing ->
       putStrLn "Couldn't decode Person"
     Just p ->
-      putStrLn $ "Hi " ++ (name p)
+      putStrLn $ "Hi " ++ (first_name p)
